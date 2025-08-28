e47a8378ae23438dc0dd7adb91b1ccb8d656a489
Arctic Fox <tyskanhik@mail.ru>
2025-08-29 01:29:09 +0300

    create click

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { afterNextRender, Component, ElementRef, signal, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { buffer, debounceTime, filter, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('RxJs');
+
+  clickDiv = viewChild<ElementRef<HTMLDivElement>>('clickDiv');
+
+  lastAction = signal<string>('None');
+
+  constructor() {
+    afterNextRender(() => {
+      this.setupClickHandling();
+    });
+  }
+
+  private setupClickHandling() {
+    const click$ = fromEvent(this.clickDiv()!.nativeElement, 'click');
+
+    const bufferedClicks$ = click$.pipe(
+      buffer(click$.pipe(debounceTime(300))),
+      filter(clicks => clicks.length < 3)
+    );
+
+    bufferedClicks$.subscribe(clicks => {
+      if (clicks.length === 1) {
+        this.handleSingleClick();
+      } else if (clicks.length === 2) {
+        this.handleDoubleClick();
+      }
+    });
+  }
+
+  private handleSingleClick() {
+    this.lastAction.set('Single Click');
+    console.log('Single click action executed');
+  }
+
+  private handleDoubleClick() {
+    this.lastAction.set('Double Click');
+    console.log('Double click action executed');
+  }
 }
