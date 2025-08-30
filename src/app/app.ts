b35669158778204fe46dbb02231a8be5897b9937
Arctic Fox <tyskanhik@mail.ru>
2025-08-30 21:23:31 +0300

    add unsubscribe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RxJsSevice } from './service/rx-js-sevice';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,22 @@ import { RxJsSevice } from './service/rx-js-sevice';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnDestroy{
   protected readonly title = signal('RxJs');
   protected service = inject(RxJsSevice);
+  private destroy$ = new Subject<void>();
 
   loadData() {
-    this.service.getUserData().subscribe({
+    this.service.getUserData().pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe({
       next: res => console.log(res)
     })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
